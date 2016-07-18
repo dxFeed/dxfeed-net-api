ca1a946036629f88c893c0a888d65247aeacb0e7
Nikita Avdeev <nikita.avdeev@simbirsoft.com>
2016-07-18 17:18:54 +0400

    Update assembly info for samples

--- samples/dxf_candle_sample/Properties/AssemblyInfo.cs
+++ samples/dxf_candle_sample/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("dxf_candle_sample")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("dxFeed candle sample")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("DevExperts")]
 [assembly: AssemblyProduct("dxf_candle_sample")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright © 2014-2016 Devexperts LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]

c9ceb07724720e63d3c4e4c7aa5cc29fce1b2836
ruslan.mikeev <ruslan.mikeev@simbirsoft.com>
2016-07-27 14:11:59 +0400

    New classes

---
+++ dxf_api/src/ipf/option/OptionChain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.dxfeed.ipf.option {
+	class OptionChain {
+		/// <summary>
+		///Returns symbol(product or underlying) of this option chain.
+		/// </summary>
+		/// <returns>Symbol (product or underlying) of this option chain.</returns>
+		public string Symbol {
+			get; private set;
+		}
+
+		OptionChain(string symbol) {
+			Symbol = symbol;
+		}
+
+	}
+}
---
+++ dxf_api/src/ipf/option/OptionSeries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.dxfeed.ipf.option {
+	class OptionSeries {
+
+		public int Expiration {
+			/// <summary>
+			///Returns day id of expiration.
+			///Example: {@link DayUtil#getDayIdByYearMonthDay DayUtil.getDayIdByYearMonthDay}(20090117).
+			/// </summary>
+			///<returns>Day id of expiration.</returns>
+			get;
+		}
+
+	}
+}

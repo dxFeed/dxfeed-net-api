5d530ba901d4995fecf2d3846fc1b1351c9c78f0
Nikita Avdeev <nikita.avdeev@simbirsoft.com>
2016-07-28 08:37:09 +0400

    Add instrument profile sample

---
+++ samples/dxf_instrument_profile_sample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using com.dxfeed.ipf;
+
+namespace dxf_instrument_profile_sample {
+    class Program {
+        static void Main(string[] args) {
+
+            const string dxfToolsUser = "demo";
+            const string dxfToolsPassword = "demo";
+            const string dxfToolsHost = "https://tools.dxfeed.com/ipf";
+            InstrumentProfileReader reader = new InstrumentProfileReader();
+            try {
+                IList<InstrumentProfile> profiles = reader.ReadFromFile(dxfToolsHost, dxfToolsUser, dxfToolsPassword);
+                Console.WriteLine("Profiles count: " + profiles.Count);
+            } catch (Exception exc) {
+                Console.WriteLine("Exception occured: " + exc.ToString());
+            }
+
+        }
+    }
+}

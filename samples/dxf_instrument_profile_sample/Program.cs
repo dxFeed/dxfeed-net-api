d0d2d6f57912d2940ade0ad90c0e07c9593ced24
Nikita Avdeev <nikita.avdeev@simbirsoft.com>
2016-08-01 16:44:47 +0400

    Update instrument profile sample

--- samples/dxf_instrument_profile_sample/Program.cs
+++ samples/dxf_instrument_profile_sample/Program.cs
@@ -5,40 +5,72 @@ using com.dxfeed.ipf;
 
 namespace dxf_instrument_profile_sample {
     class Program {
+
+        static bool IsFilePath(string path) {
+            try {
+                return new Uri(path).IsFile;
+            } catch (UriFormatException) {
+                //This exception used for determine that path is not valid uri.
+                return true;
+            }
+        }
+
+        static int MAX_PRINT_COUNT = 7;
+
         static void Main(string[] args) {
 
-            const string dxfToolsUser = "demo";
-            const string dxfToolsPassword = "demo";
-            const string dxfToolsHost = "https://tools.dxfeed.com/ipf";
-            const string TXT_FILE_PATH = "profiles.txt";
+            if (args.Length == 0) {
+                Console.WriteLine(
+                    "Usage: dxf_instrument_profile_sample <host> <user> <password>\n" +
+                    "or:    dxf_instrument_profile_sample <file>\n" +
+                    "where\n" +
+                    "    host      - valid host to download instruments (https://tools.dxfeed.com/ipf)\n" +
+                    "    user      - user name to host access\n" +
+                    "    password  - user password to host access\n" +
+                    "    file      - name of file or archive (.gz or .zip) contains instrument profiles\n" +
+                    "example: dxf_instrument_profile_sample https://tools.dxfeed.com/ipf demo demo\n" + 
+                    "or:      dxf_instrument_profile_sample profiles.zip\n"
+                );
+                return;
+            }
+
+            string path = args[0];
+            string user = string.Empty;
+            string password = string.Empty;
+            IList<InstrumentProfile> profiles = null;
             const string ZIP_FILE_PATH = "profiles.zip";
-            const string GZ_FILE_PATH = "profiles.gz";
-
-            InstrumentProfileReader reader = new InstrumentProfileReader();
-            InstrumentProfileWriter writer = new InstrumentProfileWriter();
             try {
-                IList<InstrumentProfile> profiles = reader.ReadFromFile(dxfToolsHost, dxfToolsUser, dxfToolsPassword);
-                Console.WriteLine("Profiles count: " + profiles.Count);
-                writer.WriteToFile(TXT_FILE_PATH, profiles);
-                writer.WriteToFile(ZIP_FILE_PATH, profiles);
-                writer.WriteToFile(GZ_FILE_PATH, profiles);
+                InstrumentProfileReader reader = new InstrumentProfileReader();
+                if (IsFilePath(path)) {
+                    //Read profiles from local file system
+                    using (FileStream inputStream = new FileStream(path, FileMode.Open)) {
+                        profiles = reader.Read(inputStream, path);
+                    }
+                } else {
+                    if (args.Length >= 2)
+                        user = args[1];
+                    if (args.Length >= 3)
+                        password = args[2];
+                    //Read profiles from server
+                    profiles = reader.ReadFromFile(path, user, password);
+                }
 
-                using (FileStream inputStream = new FileStream(TXT_FILE_PATH, FileMode.Open)) {
-                    IList<InstrumentProfile> txtProfiles = reader.Read(inputStream, TXT_FILE_PATH);
-                    Console.WriteLine("Profiles from TXT count: " + txtProfiles.Count);
-                }
-                using (FileStream inputStream = new FileStream(ZIP_FILE_PATH, FileMode.Open)) {
-                    IList<InstrumentProfile> txtProfiles = reader.Read(inputStream, ZIP_FILE_PATH);
-                    Console.WriteLine("Profiles from ZIP count: " + txtProfiles.Count);
-                }
-                using (FileStream inputStream = new FileStream(GZ_FILE_PATH, FileMode.Open)) {
-                    IList<InstrumentProfile> txtProfiles = reader.Read(inputStream, GZ_FILE_PATH);
-                    Console.WriteLine("Profiles from GZIP count: " + txtProfiles.Count);
-                }
+                //Iterate through received profiles
+                Console.WriteLine(String.Format("Profiles from '{0}' count: {1}", path, profiles.Count));
+                Console.WriteLine(String.Format("Print first {0} instruments:", MAX_PRINT_COUNT));
+                for (int i = 0; i < Math.Min(profiles.Count, MAX_PRINT_COUNT); i++)
+                    Console.WriteLine(String.Format("#{0}:{1}", i, profiles[i].ToString()));
+                if (profiles.Count > MAX_PRINT_COUNT)
+                    Console.WriteLine(String.Format("   {0} instruments left...", profiles.Count - MAX_PRINT_COUNT));
+
+                //Write profiles to local file system
+                InstrumentProfileWriter writer = new InstrumentProfileWriter();
+                writer.WriteToFile(ZIP_FILE_PATH, profiles);
             } catch (Exception exc) {
                 Console.WriteLine("Exception occured: " + exc.ToString());
             }
 
         }
+
     }
 }

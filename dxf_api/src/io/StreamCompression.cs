ed5846febbfb4199fda4a95bfa0e86f13bd6794e
Nikita Avdeev <nikita.avdeev@simbirsoft.com>
2016-08-26 13:23:16 +0400

    Update comments

--- dxf_api/src/io/StreamCompression.cs
+++ dxf_api/src/io/StreamCompression.cs
@@ -6,24 +6,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace com.dxfeed.io {
+namespace com.dxfeed.io
+{
+
+    /// <summary>
+    /// Defines byte stream compression format.
+    ///
+    /// Supported compression formats are: NONE, GZIP(.gz), ZIP(.zip).
+    /// NONE compression format serves as a null object and does not do anything.
+    /// </summary>
     public class StreamCompression {
 
+        /// <summary>
+        /// Supported compression types.
+        /// </summary>
         public enum CompressionType { None, Gzip, Zip};
 
-        /**
-         * No compression.
-         */
+        /// <summary>
+        /// No compression.
+        /// </summary>
         public static StreamCompression NONE = new StreamCompression(CompressionType.None, "none", "");
 
-        /**
-         * Gzip compression format.
-         */
+        /// <summary>
+        /// Gzip compression format.
+        /// </summary>
         public static StreamCompression GZIP = new StreamCompression(CompressionType.Gzip, "gzip", ".gz");
 
-        /**
-         * Zip compression format.
-         */
+        /// <summary>
+        /// Zip compression format.
+        /// </summary>
         public static StreamCompression ZIP = new StreamCompression(CompressionType.Zip, "zip", ".zip");
 
         private CompressionType type;
@@ -36,13 +47,12 @@ namespace com.dxfeed.io {
             this.extension = extension;
         }
 
-        /**
-         * Detects compression format by the extension at the end of the file name.
-         *
-         * @param fileName the file name.
-         * @return detected compression format or {@link #NONE} is the file name extension is not recognized.
-         * @throws NullPointerException if {@code fileName} is {@code null}.
-         */
+        /// <summary>
+        /// Detects compression format by the extension at the end of the file name.
+        /// </summary>
+        /// <param name="fileName">The file name.</param>
+        /// <returns>Detected compression format or NONE is the file name extension is not recognized.</returns>
+        /// <exception cref="ArgumentException">If fileName is null.</exception>
         public static StreamCompression DetectCompressionByExtension(string fileName) {
             string ext = Path.GetExtension(fileName).ToLower();
             if (ext.Equals(GZIP.extension))
@@ -52,80 +62,92 @@ namespace com.dxfeed.io {
             return NONE;
         }
 
-        /**
-         * Detects compression format by the extension at the end of the file name.
-         *
-         * @param fileName the file name.
-         * @return detected compression format or {@link #NONE} is the file name extension is not recognized.
-         * @throws NullPointerException if {@code fileName} is {@code null}.
-         */
+        /// <summary>
+        /// Detects compression format by the extension at the end of the file name.
+        /// </summary>
+        /// <param name="fileUri">The file name Uri object.</param>
+        /// <returns>Detected compression format or NONE is the file name extension is not recognized.</returns>
+        /// <exception cref="ArgumentException">If fileUri is null.</exception>
         public static StreamCompression DetectCompressionByExtension(Uri fileUri) {
+            if (fileUri == null)
+                throw new ArgumentException("File Uri is null");
             return DetectCompressionByExtension(fileUri.AbsolutePath);
         }
 
-        /**
-         * Detects compression format by the magic number in the file header. This method
-         * {@link InputStream#mark(int) marks} the stream, read first 4 bytes, and
-         * {@link InputStream#reset() resets} the stream to an original state.
-         *
-         * @param in the input stream.
-         * @return detected compression format or {@link #NONE} is the header is not recognized.
-         * @throws IOException              if an I/O error occurs.
-         * @throws IllegalArgumentException if {@code in} does not {@link InputStream#markSupported() support mark}.
-         */
+        /// <summary>
+        /// Detects compression format by the magic number in the file header. This method
+        /// marks the stream, read first 4 bytes, and resets the stream to an original state.
+        /// </summary>
+        /// <param name="inputStream">The input stream.</param>
+        /// <returns>Detected compression format or NONE is the header is not recognized.</returns>
+        /// <exception cref="IOException">If an I/O error occurs.</exception>
+        /// <exception cref="InvalidOperationException"></exception>
         public static StreamCompression DetectCompressionByHeader(Stream inputStream) {
             int n = 4;
             byte[] buffer = new byte[n];
             int mark = n;
-            int read = inputStream.Read(buffer, 0, n);
-            inputStream.Position = mark;
-            if (read != n)
+            try {
+                int read = inputStream.Read(buffer, 0, n);
+                inputStream.Position = mark;
+                if (read != n)
+                    return NONE;
+                if (buffer[0] == (byte)0x1f && buffer[1] == (byte)0x8b)
+                    return GZIP;
+                if (buffer[0] == 'P' && buffer[1] == 'K' && buffer[2] == 0x03 && buffer[3] == 0x04)
+                    return ZIP;
                 return NONE;
-            if (buffer[0] == (byte)0x1f && buffer[1] == (byte)0x8b)
-                return GZIP;
-            if (buffer[0] == 'P' && buffer[1] == 'K' && buffer[2] == 0x03 && buffer[3] == 0x04)
-                return ZIP;
-            return NONE;
+            } catch (IOException) {
+                throw;
+            } catch (Exception e) {
+                throw new InvalidOperationException("Detect compression exception: " + e);
+            }
         }
 
-        /**
-         * Detects compression format by the magic number in the file header and decompresses
-         * the given input stream. This method wraps the input stream in {@link BufferedInputStream} if
-         * the original stream does not {@link InputStream#markSupported() support mark} before using
-         * {@link #detectCompressionByHeader(InputStream) detectCompressionByHeader} method.
-         *
-         * @param in the input stream.
-         * @return the decompressed stream.
-         * @throws IOException if an I/O error occurs.
-         */
+        /// <summary>
+        /// Detects compression format by the magic number in the file header and decompresses
+        /// the given input stream. 
+        /// </summary>
+        /// <param name="inputStream">In the input stream.</param>
+        /// <returns>The decompressed stream.</returns>
+        /// <exception cref="ArgumentException">If input stream is null.</exception>
+        /// <exception cref="IOException">If an I/O error occurs.</exception>
+        /// <exception cref="InvalidOperationException"></exception>
         public static Stream DetectCompressionByHeaderAndDecompress(Stream inputStream) {
             return DetectCompressionByHeader(inputStream).Decompress(inputStream);
         }
 
-        /**
-         * Decompresses the given input stream with this compression format.
-         *
-         * @param in the input stream.
-         * @return the decompressed stream or an original stream if this compression format is {@link #NONE}.
-         * @throws IOException if an I/O error occurs.
-         */
+        /// <summary>
+        /// Decompresses the given input stream with this compression format.
+        /// </summary>
+        /// <param name="inputStream">In the input stream.</param>
+        /// <returns>The decompressed stream or an original stream if this compression format is NONE.</returns>
+        /// <exception cref="ArgumentException">If input stream is null.</exception>
+        /// <exception cref="IOException">If an I/O error occurs.</exception>
         public Stream Decompress(Stream inputStream) {
-            switch (type) {
-                case CompressionType.None:
+            if (inputStream == null)
+                throw new ArgumentException("Input stream is null.");
+            try {
+                switch (type) {
+                    case CompressionType.None:
                         return inputStream;
-                case CompressionType.Gzip:
-                    return new GZipStream(inputStream, CompressionMode.Decompress);
-                case CompressionType.Zip:
-                    MemoryStream zipStream = new MemoryStream();
-                    using (ZipArchive zip = new ZipArchive(inputStream)) {
-                        foreach (ZipArchiveEntry entry in zip.Entries) {
-                            entry.Open().CopyTo(zipStream);
+                    case CompressionType.Gzip:
+                        return new GZipStream(inputStream, CompressionMode.Decompress);
+                    case CompressionType.Zip:
+                        MemoryStream zipStream = new MemoryStream();
+                        using (ZipArchive zip = new ZipArchive(inputStream)) {
+                            foreach (ZipArchiveEntry entry in zip.Entries) {
+                                entry.Open().CopyTo(zipStream);
+                            }
                         }
-                    }
-                    zipStream.Position = 0;
-                    return zipStream;
-                default:
-                    throw new InvalidOperationException("Unsupported compression type: " + type);
+                        zipStream.Position = 0;
+                        return zipStream;
+                    default:
+                        throw new InvalidOperationException("Unsupported compression type: " + type);
+                }
+            } catch (IOException) {
+                throw;
+            } catch (Exception e) {
+                throw new IOException("Internal error:" + e);
             }
         }
 

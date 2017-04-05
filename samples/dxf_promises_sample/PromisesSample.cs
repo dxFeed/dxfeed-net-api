e0ebfd6ac6f942f9efbe3d0a14c39389f16a6cf0
nikita.avdeev <nikita.avdeev@simbirsoft.com>
2017-04-05 16:18:18 +0400

    Implement additional promise sample

---
+++ samples/dxf_promises_sample/PromisesSample.cs
@@ -0,0 +1,80 @@
+﻿#region License
+// Copyright (C) 2010-2016 Devexperts LLC
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at
+// http://mozilla.org/MPL/2.0/.
+#endregion
+
+using com.dxfeed.api;
+using com.dxfeed.api.events;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace com.dxfeed.samples
+{
+    class PromisesSample
+    {
+        static void GetLastEventPromisesSample()
+        {
+            string[] symbols = new string[] { "C", "IBM", "MSFT" };
+            DXFeed feed = DXFeed.GetInstance();
+            List<Task<LastingEvent>> promises = feed.GetLastEventsPromises<IDxTrade>(
+                symbols,
+                new CancellationTokenSource(TimeSpan.FromSeconds(5)).Token);
+            // combine the list of promises into one with Task utility method and wait
+            try
+            {
+                Task.WaitAll(promises.ToArray());
+            }
+            catch (AggregateException) { }
+            // now iterate the promises to retrieve results
+            Console.WriteLine(string.Format("Last events for {0} symbols:", string.Join(", ", symbols)));
+            foreach (var promise in promises)
+                // result received exceptionally if this event was not found
+                // so first check that task completes successfully
+                if (promise.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine(promise.Result);
+                else
+                    Console.WriteLine("not found");
+        }
+
+        static void GetIndexedEventsPromise()
+        {
+            DXFeed feed = DXFeed.GetInstance();
+            var tsPromise = feed.GetIndexedEventsPromise<IDxTimeAndSale>("IBM", IndexedEventSource.DEFAULT,
+                new CancellationTokenSource(TimeSpan.FromSeconds(2)).Token);
+            Console.WriteLine("TimeAndSale events:");
+            try
+            {
+                foreach (var result in tsPromise.Result)
+                    Console.WriteLine(result);
+            }
+            catch (AggregateException)
+            {
+                Console.WriteLine("not found");
+            }
+
+            var orderPromise = feed.GetIndexedEventsPromise<IDxOrder>("IBM", OrderSource.NTV,
+                new CancellationTokenSource(TimeSpan.FromSeconds(2)).Token);
+            Console.WriteLine("Order#NTV events:");
+            try
+            {
+                foreach (var result in orderPromise.Result)
+                    Console.WriteLine(result);
+            }
+            catch (AggregateException)
+            {
+                Console.WriteLine("not found");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            GetLastEventPromisesSample();
+            GetIndexedEventsPromise();
+        }
+    }
+}

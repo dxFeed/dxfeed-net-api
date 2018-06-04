2f5d551792ef853aba2c6b7a882f58a9fc31e31e
Roman Lugansky <lugansky@devexperts.com>
2018-06-04 12:44:51 +0300

    Fix exception in hex formating with padding of enums

--- dxf_native/src/events/NativeGreeks.cs
+++ dxf_native/src/events/NativeGreeks.cs
@@ -66,7 +66,7 @@ namespace com.dxfeed.native.events
                 "Theta: {9}, Vega: {10}"                 +
                 "}}",
                 EventSymbol,
-                EventFlags, Index,
+                (int) EventFlags, Index,
                 Time,
                 Price, Volatility,
                 Delta, Gamma, Rho,

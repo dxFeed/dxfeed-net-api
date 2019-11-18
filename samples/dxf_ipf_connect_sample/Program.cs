daa643232fcd15416a8e2256857cdb77e2205882
ttldtor <ttldtor@gmail.com>
2019-11-18 14:19:39 +0300

    [DXFC-152] dxf_ipf_connect_sample

---
+++ samples/dxf_ipf_connect_sample/Program.cs
@@ -0,0 +1,26 @@
+﻿#region License
+
+/*
+Copyright © 2010-2019 dxFeed Solutions DE GmbH
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dxf_ipf_connect_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

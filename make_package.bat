0d7ff00048c89832fde14bc25d33ab8da6d0e7c8
ttldtor <ttldtor@gmail.com>
2020-08-24 14:19:56 +0300

    Update make_package.bat

--- make_package.bat
+++ make_package.bat
@@ -67,6 +67,7 @@ set C_API_SCRIPT_NAME=make_package.bat
 set C_API_BUILD=%C_API_PATH%\%C_API_SCRIPT_NAME%
 set C_API_LIB_NAME=DXFeed
 set C_API_LIB_EXT=.dll
+set C_API_LIB_PDB_EXT=.pdb
 set TARGET_TEST=RunUnitTests;
 set C_API_NO_TEST=
 set C_API_NO_BUIILD=
@@ -119,11 +120,19 @@ if %C_API_ERRORLEVEL% GEQ 1 (
 :copy_c
 xcopy /Y /I %C_API_PATH%\build\x86\Release\%C_API_LIB_NAME%%C_API_LIB_EXT% %~dp0\lib
 if %ERRORLEVEL% GEQ 1 goto exit_error
+xcopy /Y /I %C_API_PATH%\build\x86\Release\%C_API_LIB_NAME%%C_API_LIB_PDB_EXT% %~dp0\lib
+if %ERRORLEVEL% GEQ 1 goto exit_error
 xcopy /Y /I %C_API_PATH%\build\x86\Debug\%C_API_LIB_NAME%d%C_API_LIB_EXT% %~dp0\lib
+if %ERRORLEVEL% GEQ 1 goto exit_error
+xcopy /Y /I %C_API_PATH%\build\x86\Debug\%C_API_LIB_NAME%d%C_API_LIB_PDB_EXT% %~dp0\lib
 if %ERRORLEVEL% GEQ 1 goto exit_error
 xcopy /Y /I %C_API_PATH%\build\x64\Release\%C_API_LIB_NAME%_64%C_API_LIB_EXT% %~dp0\lib
 if %ERRORLEVEL% GEQ 1 goto exit_error
+xcopy /Y /I %C_API_PATH%\build\x64\Release\%C_API_LIB_NAME%_64%C_API_LIB_PDB_EXT% %~dp0\lib
+if %ERRORLEVEL% GEQ 1 goto exit_error
 xcopy /Y /I %C_API_PATH%\build\x64\Debug\%C_API_LIB_NAME%d_64%C_API_LIB_EXT% %~dp0\lib
+if %ERRORLEVEL% GEQ 1 goto exit_error
+xcopy /Y /I %C_API_PATH%\build\x64\Debug\%C_API_LIB_NAME%d_64%C_API_LIB_PDB_EXT% %~dp0\lib
 if %ERRORLEVEL% GEQ 1 goto exit_error
 
 rem === BUILD PROJECTS ===

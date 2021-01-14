5bf0637ec6f08782dc9d8155171e57e4b5492539
ttldtor <ttldtor@gmail.com>
2021-01-14 20:17:18 +0300

    [DXFC-181] Extend Underlying and Series events with putVolume and callVolume fields (#26)

--- dxf_api/src/events/IDxUnderlying.cs
+++ dxf_api/src/events/IDxUnderlying.cs
@@ -33,6 +33,18 @@ namespace com.dxfeed.api.events
         /// </summary>
         double BackVolatility { get; }
         /// <summary>
+        /// Returns call options traded volume for a day
+        /// </summary>
+        double CallVolume { get; }
+        /// <summary>
+        /// Returns put options traded volume for a day
+        /// </summary>
+        double PutVolume { get; }
+        /// <summary>
+        /// Returns options traded volume for a day
+        /// </summary>
+        double OptionVolume { get; }
+        /// <summary>
         /// Returns ratio of put traded volume to call traded volume for a day.
         /// </summary>
         double PutCallRatio { get; }

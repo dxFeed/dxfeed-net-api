ae78da9a42315c3f9149b269264a34f3e8dea2c4
Anatoly Kalin <ttldtor@gmail.com>
2021-06-22 19:16:01 +0300

    [DXFC-215] Rename IDxFundamentalListener to IDxSummaryListener

---
+++ dxf_api/src/listeners/event/IDxSummaryListener.cs
@@ -0,0 +1,31 @@
+﻿#region License
+
+/*
+Copyright (c) 2010-2021 Devexperts LLC
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#endregion
+
+using com.dxfeed.api.events;
+
+namespace com.dxfeed.api
+{
+    /// <summary>
+    /// Interface provides receiving Summary events.
+    /// </summary>
+    public interface IDxSummaryListener : IDxEventListener
+    {
+        /// <summary>
+        /// On Summary event received.
+        /// </summary>
+        /// <typeparam name="TB">Event buffer type.</typeparam>
+        /// <typeparam name="TE">Event type.</typeparam>
+        /// <param name="buf">Event buffer object.</param>
+        void OnSummary<TB, TE>(TB buf)
+            where TB : IDxEventBuf<TE>
+            where TE : IDxSummary;
+    }
+}

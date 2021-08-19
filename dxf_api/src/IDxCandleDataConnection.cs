6d2680809c45a3bf16f9d1b14ce773218b5c8283
Anatoly Kalin <ttldtor@gmail.com>
2021-08-19 23:01:01 +0300

    CandleDataConnection

---
+++ dxf_api/src/IDxCandleDataConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using com.dxfeed.api.candle;
+using com.dxfeed.api.events;
+
+namespace com.dxfeed.api
+{
+    /// <summary>
+    /// Interface provides operations with candle data retrieving
+    /// </summary>
+    public interface IDxCandleDataConnection : IDisposable
+    {
+        /// <summary>
+        /// Asynchronously returns a "snapshot" of candle data for the specified symbols and period.
+        /// </summary>
+        /// <param name="symbols">The candle symbols</param>
+        /// <param name="fromTime"></param>
+        /// <param name="toTime"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>The task for the result of the request.</returns>
+        Task<List<IDxCandle>> GetCandleData(CandleSymbol[] symbols, DateTime fromTime, DateTime toTime,
+            CancellationToken cancellationToken);
+    }
+}
---
+++ dxf_native/src/CandleDataConnection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using com.dxfeed.api;
+using com.dxfeed.api.candle;
+using com.dxfeed.api.events;
+
+namespace com.dxfeed.native
+{
+    /// <summary>
+    /// Class provides operations with candle data retrieving
+    /// </summary>
+    public class CandleDataConnection : IDxCandleDataConnection
+    {
+        private string address;
+        private NetworkCredential credential;
+        
+        public CandleDataConnection(string address, NetworkCredential credential)
+        {
+            this.address = address;
+            this.credential = credential;
+        }
+        
+        public CandleDataConnection(string address)
+        {
+            this.address = address;
+            this.credential = null;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public Task<List<IDxCandle>> GetCandleData(CandleSymbol[] symbols, DateTime fromTime, DateTime toTime, CancellationToken cancellationToken)
+        {
+            return Task.Run(async ()  =>
+            {
+                var result = new List<IDxCandle>();
+
+                using (var httpClient = new HttpClient())
+                {
+                    var builder = new UriBuilder(address);
+
+                    var stream = await httpClient.GetStreamAsync(address);
+                }
+
+                return result;
+            }, cancellationToken);
+        }
+    }
+}
